1b78bdacf37853b2048be23757d38f9cde768a4f
Demin <deminpe@otd263>
2011-10-26 14:31:29 +0400

    expand fix for nested tables

--- DoDoc_parameters_parser.py
+++ DoDoc_parameters_parser.py
@@ -236,7 +236,7 @@ def readFile(filename, forced_encoding = None):
 def parseParameters_XML(xml_content):
     p = Parser()
     xml.sax.parseString(xml_content, p)
-    #return expandImages_in_tables(p.tree)
+    return expandImages_in_tables(p.tree)
     return p.tree
 
 def is_table(value):
@@ -259,42 +259,49 @@ def is_image(value):
         return False
     return False
 
+def expandTable(table):
+    id = 1
+    sub_id = 1
+    expanded = []
+    for row in table:
+        image_rows = 0
+        row['ID'] = unicode(id)
+        row['AMOUNT'] = unicode(len(table))
+        for cell in row.itervalues():
+            if is_image(cell):
+                #print rk, 'in', k, len(rv)
+                image_rows = max(len(cell), image_rows)
+        if image_rows in (0, 1):
+            # Do not inject sub ids single row
+            #row['SUB_ID'] = unicode(1)
+            #row['SUB_AMOUNT'] = unicode(1)
+            expanded.append(row)
+        else:
+            for image_id in range(image_rows):
+                cur_row = {}
+                for rk, rv in row.iteritems():
+                    if is_image(rv):
+                        if len(rv) > image_id:
+                            cur_row[rk] = [rv[image_id]]
+                        else:
+                            cur_row[rk] = []
+                    elif is_table(rv):
+                        cur_row[rk] = expandTable(rv)
+                    else:
+                        cur_row[rk] = rv
+                cur_row['SUB_ID'] = unicode(sub_id)
+                cur_row['SUB_AMOUNT'] = unicode(image_rows)
+                expanded.append(cur_row)
+                sub_id+= 1
+        id += 1
+        sub_id = 1
+    return expanded
+
 def expandImages_in_tables(params):
     result = {}
     for k, v in params.iteritems():
         if is_table(v):
-            id = 1
-            sub_id = 1
-            result[k] = []
-            for row in v:
-                image_rows = 0
-                row['ID'] = unicode(id)
-                row['AMOUNT'] = unicode(len(v))
-                for rk, rv in row.iteritems():
-                    if is_image(rv):
-                        #print rk, 'in', k, len(rv)
-                        image_rows = max(len(rv), image_rows)
-                if image_rows in (0, 1):
-                    #row['SUB_ID'] = unicode(1)
-                    #row['SUB_AMOUNT'] = unicode(1)
-                    result[k].append(row)
-                else:
-                    for image_id in range(image_rows):
-                        cur_row = {}
-                        for rk, rv in row.iteritems():
-                            if is_image(rv):
-                                if len(rv) > image_id:
-                                    cur_row[rk] = [rv[image_id]]
-                                else:
-                                    cur_row[rk] = []
-                            else:
-                                cur_row[rk] = rv
-                        cur_row['SUB_ID'] = unicode(sub_id)
-                        cur_row['SUB_AMOUNT'] = unicode(image_rows)
-                        result[k].append(cur_row)
-                        sub_id+= 1
-                id += 1
-                sub_id = 1
+            result[k] = expandTable(v)
         else:
             result[k] = v
     return result
@@ -335,7 +342,7 @@ def main():
             <a>3</a>
             <flow_chart type='image'>
                 <IMAGE>1.png</IMAGE>
-                <IMAGE>2.png</IMAGE>
+                <IMAGE>1.png</IMAGE>
             </flow_chart>
         </ROW>
         <ROWS_FROM_CSV filename="example.csv"/>
