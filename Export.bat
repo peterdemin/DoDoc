219b063f1f8d9cd3f59d9ebce61fe11e1b9265a2
Demin <deminpe@otd263>
2011-11-02 10:04:50 +0400

    version inc

--- Export.bat
+++ Export.bat
@@ -7,4 +7,6 @@ copy /Y odg2png.py                  Export\odg2png.py
 copy /Y odt2pdf.py                  Export\odt2pdf.py
 copy /Y OpenOffice_document.py      Export\OpenOffice_document.py
 copy /Y Template.py                 Export\Template.py
+copy /Y DoDoc_styles.py             Export\DoDoc_styles.py
+copy /Y DoXML.py                    Export\DoXML.py
 copy /Y README.txt                  Export\README.txt
