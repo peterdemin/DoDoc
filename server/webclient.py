f7aad7ce65a14c0fd4e4b60bb3af39ba4674bbc4
Demin <deminpe@otd263>
2011-12-22 17:22:24 +0400

    Better server v1.0a

--- server/webclient.py
+++ server/webclient.py
@@ -1,31 +1,98 @@
-import email.message
-from email.mime.text import MIMEText
-from email.mime.multipart import MIMEMultipart
-from email.mime.application import MIMEApplication
-import email.parser
-import urllib2
-import json
-
-filename = 'koi_h.odg'
-
-msg = MIMEMultipart()
-payload = MIMEApplication(open(filename, 'rb').read())
-payload.add_header('Content-Disposition', 'form-data', filename=filename, name='upfile')
-msg.attach(payload)
-
-headers, body = {}, ''
-in_headers = True
-for line in msg.as_string().splitlines():
-    if in_headers:
-        if len(line.strip()) == 0:
-            in_headers = False
-        else:
-            item = line.split(':', 1)
-            headers[item[0].strip()] = item[1].strip()
-    else:
-        body+= line + '\n'
-
+import os
+import httplib
+import mimetypes
+import urlparse
 from pprint import pprint
-pprint(headers)
-request = urllib2.Request('http://localhost/add/', headers=headers)
-pprint( json.load(urllib2.urlopen(request, body)) )
+import json
+import urllib2
+
+def postForm_data(url, fields, files):
+    """
+    Post fields and files to an http host as multipart/form-data.
+    fields is a sequence of (name, value) elements for regular form fields.
+    files is a sequence of (name, filename, value) elements for data to be uploaded as files
+    Return the server's response page.
+    """
+    host, selector = urlparse.urlsplit(url)[1:3]
+    content_type, body = composeForm_data(fields, files)
+    h = httplib.HTTP(host)
+    h.putrequest('POST', selector)
+    h.putheader('content-type', content_type)
+    h.putheader('content-length', str(len(body)))
+    h.endheaders()
+    h.send(body)
+    errcode, errmsg, headers = h.getreply()
+    return h.file.read()
+
+def postComposed(url, data):
+    host, selector = urlparse.urlsplit(url)[1:3]
+    content_type, body = data
+    h = httplib.HTTP(host)
+    h.putrequest('POST', selector)
+    h.putheader('content-type', content_type)
+    h.putheader('content-length', str(len(body)))
+    h.endheaders()
+    h.send(body)
+    errcode, errmsg, headers = h.getreply()
+    return h.file.read()
+
+def composeForm_data(fields, files):
+    """
+    fields is a sequence of (name, value) elements for regular form fields.
+    files is a sequence of (name, filename, value) elements for data to be uploaded as files
+    Return (content_type, body) ready for httplib.HTTP instance
+    """
+    BOUNDARY = '----------ThIs_Is_tHe_bouNdaRY_$'
+    CRLF = '\n'
+    L = []
+    for (key, value) in fields:
+        L.append('--' + BOUNDARY)
+        L.append('Content-Disposition: form-data; name="%s"' % key)
+        L.append('')
+        L.append(value)
+    for (key, filename, value) in files:
+        L.append('--' + BOUNDARY)
+        L.append('Content-Disposition: form-data; name="%s"; filename="%s"' % (key, filename))
+        L.append('Content-Type: %s' % getContent_type(filename))
+        L.append('')
+        L.append(value)
+    L.append('--' + BOUNDARY + '--')
+    L.append('')
+    body = CRLF.join(L)
+    content_type = 'multipart/form-data; boundary=%s' % BOUNDARY
+    return content_type, body
+
+def getContent_type(filename):
+    return mimetypes.guess_type(filename)[0] or 'application/octet-stream'
+
+filenames = ['navig.odg',
+             'bshv000_SpkRG.odg',
+             'bshv001_SpkRG.odg',
+             'bshv010_SpkRG.odg',
+             'bshv020_SpkRG.odg',
+             'koi_h.odg',
+            ]
+
+#session = json.loads(postForm_data('http://127.0.0.1/add/', [], [('upfile', name, open(name, 'rb').read()) for name in filenames]))
+
+for i in range(10):
+    #trash = urllib2.urlopen('http://www.mars/asu/index.html').read()
+    trash = urllib2.urlopen('http://localhost/ping/').read()
+    print 'ping'
+
+datas = []
+for filename in filenames:
+    datas.append(composeForm_data([], [('upfile', filename, open(filename, 'rb').read())]))
+    print '.',
+print
+
+session = {}
+for data in datas:
+    cur_session = json.loads(postComposed('http://127.0.0.1/add/', data))
+    #session.update(cur_session)
+    #pprint(cur_session)
+command = {'template' : 'TT.odt', 'xml' : 'koi_h_TT.xml'}
+result = json.loads(postForm_data('http://127.0.0.1/dodoc/', [('session', json.dumps(session)), ('command', json.dumps(command))], []))
+pprint(result)
+open('result.odt', 'wb').write(urllib2.urlopen('http://127.0.0.1/' + result['odt']).read())
+os.startfile('result.odt')
--- server/webserver.py
+++ server/webserver.py
@@ -1,4 +1,5 @@
 import os
+import time
 import re
 import cgi
 import time
@@ -6,34 +7,13 @@ import mimetypes
 import email.parser
 import json
 import hashlib
-from BaseHTTPServer import BaseHTTPRequestHandler, HTTPServer
+import BaseHTTPServer
+from SocketServer import ThreadingMixIn
 
-class Server_API(object):
-    def __init__(self):
-        pass
-
-    def hasODG(self, filename):
-        return os.path.exists(filename)
-
-    def getODG(self, filename):
-        if hasODG(filename):
-            return open(filename, 'rb').read()
-        return None
-
-    def addODG(self, filename, content):
-        try:
-            basename = os.path.basename(filename)
-            if not os.path.isdir(basename):
-                os.mkdir(basename)
-            open(os.path.join(basename, '.odg'), 'wb').write('content')
-            return True
-        except IOError, e:
-            return False
-
-class MyHandler(BaseHTTPRequestHandler):
+class MyHandler(BaseHTTPServer.BaseHTTPRequestHandler):
     def __init__(self, *args, **kwargs):
         super(MyHandler, self).__init__(*args, **kwargs)
-        self.api = Server_API()
+        print '__init__'
 
     def getHandler(self, request_path):
         for pattern, handler in self.handlers.iteritems():
@@ -43,7 +23,6 @@ class MyHandler(BaseHTTPRequestHandler):
         return None
 
     def do_GET(self):
-        print self.path
         try:
             url_handler = self.getHandler(self.path)
             if url_handler:
@@ -62,6 +41,18 @@ class MyHandler(BaseHTTPRequestHandler):
                 self.wfile.write(f.read())
                 f.close()
                 return
+            elif self.path.endswith(".odt"):
+                filepath = os.curdir + os.sep + self.path
+                filesize = os.stat(filepath).st_size
+                f = open(filepath, 'rb')
+                self.send_response(200)
+                self.send_header('Content-Type', 'application/octet-stream')
+                self.send_header('Content-Length', str(filesize))
+                self.end_headers()
+                self.wfile.write(f.read())
+                self.wfile.flush()
+                self.connection.shutdown(1)
+                f.close()
             if self.path.endswith(".esp"):   #our dynamic content
                 self.send_response(200)
                 self.send_header('Content-type', 'text/html')
@@ -87,18 +78,18 @@ class MyHandler(BaseHTTPRequestHandler):
         else:
             print 'Can NOT handle', self.path
 
-    def addODG(self):
+    def add(self):
         size = int(self.headers['content-length'])
         content = 'content-type: %s\n\n' % (self.headers['content-type'])
         content+= self.rfile.read(size)
-        open('request.txt', 'wb').write(content)
+        open('request_add.txt', 'wb').write(content)
         message = email.parser.Parser().parsestr(content)
         payload = message.get_payload()
         new_filename = None
         self.send_response(200)
         self.end_headers()
-        post_processing = []
-        print '!'
+        post_processing = {}
+        result = {}
         for part in payload:
             parameters = dict(filter(lambda b: len(b)==2, [a.strip().split('=') for a in part['Content-Disposition'].split(';')]))
             if parameters.has_key('name') and parameters['name'] == '"upfile"':
@@ -106,64 +97,141 @@ class MyHandler(BaseHTTPRequestHandler):
                 basename, ext = os.path.splitext(filename)
                 if ext.lower() == '.odg':
                     file_content = part.get_payload(decode=True)
+                    new_dirname = hashlib.md5(file_content).hexdigest()
+                    result[filename] = new_dirname
+                    if not os.path.exists(new_dirname):
+                        os.mkdir(new_dirname)
+                        open(os.path.join(new_dirname, 'in-progress.lock'), 'wt').write('!')
+                        new_filename = os.path.join(new_dirname, filename)
+                        open(new_filename, 'wb').write(file_content)
+                        post_processing[filename] = new_filename
+                elif ext.lower() == '.xml':
+                    file_content = part.get_payload(decode=True)
                     new_filename = hashlib.md5(file_content).hexdigest()
+                    result[filename] = new_filename
                     if not os.path.exists(new_filename):
                         open(new_filename, 'wb').write(file_content)
-                        post_processing.append({filename : new_filename})
-        print len(post_processing)
-        if len(post_processing):
-            self.wfile.write(json.dumps(post_processing))
+        if len(result):
+            self.wfile.write(json.dumps(result))
         else:
             self.wfile.write(json.dumps({'error' : 'no payload'}))
         self.wfile.flush()
         self.connection.shutdown(1)
         if len(post_processing):
-            from DoDoc.odg2png import Odg2png
-            result = None
-            o = Odg2png()
-            if o.connect():
-                for origin, input in post_processing:
-                    print input
-                    if o.open(input):
-                        output = input + '.png'
-                        result = o.savePNG(output)
-                        o.close()
-            o.disconnect()
+            print post_processing.values()
+            self.convertODGs(post_processing.values())
+            for filename in post_processing.itervalues():
+                os.remove(os.path.join(os.path.dirname(filename), 'in-progress.lock'))
 
-    def hasODG(self, params):
+    def convertODGs(self, odg_filenames):
+        from DoDoc.odg2wmf import Odg2wmf
+        result = None
+        o = Odg2wmf()
+        if o.connect():
+            for input in odg_filenames:
+                print input
+                if o.open(input):
+                    output = input + '.wmf'
+                    result = o.saveWMF(output)
+                    o.close()
+        o.disconnect()
+
+    def dodoc(self):
+        size = int(self.headers['content-length'])
+        content = 'content-type: %s\n\n' % (self.headers['content-type'])
+        content+= self.rfile.read(size)
+        open('request_dodoc.txt', 'wb').write(content)
+        message = email.parser.Parser().parsestr(content)
+        payload = message.get_payload()
         self.send_response(200)
-        self.send_header('Content-type', 'text/plain')
         self.end_headers()
-        if len(params) == 1:
-            odg_md5 = params[0]
-            print odg_md5
-            if os.path.exists(odg_md5):
-                self.wfile.write('yes')
-                return
-        self.wfile.write('no')
-        return
+        result = {}
+        file_mappings, command = {}, {}
+        for part in payload:
+            parameters = dict(filter(lambda b: len(b)==2, [a.strip().split('=') for a in part['Content-Disposition'].split(';')]))
+            if parameters.has_key('name'):
+                if parameters['name'] == '"session"':
+                    file_mappings = json.loads(part.get_payload(decode=True))
+                if parameters['name'] == '"command"':
+                    command = json.loads(part.get_payload(decode=True))
+        if len(command):
+            if(file_mappings.has_key(command['xml'])):
+                command['xml'] = file_mappings[command['xml']]
+        for dirname in file_mappings.itervalues():
+            watchdog = int(50 / 0.1)
+            while not os.path.exists(dirname):
+                time.sleep(0.1)
+                watchdog-= 1
+                if watchdog == 0:
+                    return self.wfile.write(json.dumps({'error' : dirname + ' seems to be broken'}))
+            while os.path.exists(os.path.join(dirname, 'in-progress.lock')):
+                time.sleep(0.1)
+                watchdog-= 1
+                if watchdog == 0:
+                    return self.wfile.write(json.dumps({'error' : dirname + ' seems to be broken'}))
+        result_odt = '%s_%s.odt' % (os.path.splitext(os.path.basename(command['template']))[0], command['xml'])
+        result_pdf = '%s_%s.pdf' % (os.path.splitext(os.path.basename(command['template']))[0], command['xml'])
+        self.wfile.write(json.dumps({'odt' : result_odt, 'pdf' : result_pdf}))
+        self.wfile.flush()
+        self.connection.shutdown(1)
+        if not os.path.exists(result_odt):
+            from DoDoc import DoDoc
+            template_params = parseDoDoc_XML(open(command['xml'], 'rb').read(), file_mappings)
+            template = os.path.join('templates', command['template'])
+            #print result_odt
+            DoDoc.renderTemplate(template, template_params, result_odt)
+        if not os.path.exists(result_pdf):
+            #print result_pdf
+            from DoDoc.odt2pdf import odt2pdf
+            odt2pdf(result_odt, result_pdf)
 
-    def getODG(self, params):
-        if len(params) == 1:
-            filename = params[0]
-            if os.path.exists(filename):
-                self.send_response(200)
-                mime = mimetypes.guess_type(filename)[0] or 'text/plain'
-                self.send_header('Content-type', mime)
-                self.end_headers()
-                self.wfile.write(open(filename, 'rb').read())
-                return
-        self.send_error(404, 'File Not Found: %s' % self.path)
+    def ping(self):
+        self.send_response(200)
+        self.end_headers()
+        self.wfile.write('pong')
+
+    def address_string(self):
+        return self.client_address[0]
 
     handlers = {
-                re.compile(r'(?i)/has/([a-f0-9]{16}\.\w+)') : hasODG,
-                re.compile(r'(?i)/get/([a-f0-9]{16}\.\w+)') : getODG,
-                re.compile(r'(?i)/add/') : addODG,
+                re.compile(r'(?i)/dodoc/') : dodoc,
+                re.compile(r'(?i)/add/') : add,
+                re.compile(r'(?i)/ping/') : ping,
                }
+
+import xml.sax
+import glob
+from DoDoc.DoDoc_parameters_parser import Parser, expandImages_in_tables
+
+class MappingParser(Parser):
+    def __init__(self, mappings):
+        self.mappings = mappings
+        Parser.__init__(self)
+
+    def __parseODG(self, odg_path):
+        if mappings.has_key(odg_path):
+            dir_name = mappings[odg_path]
+            return glob.glob(os.path.join(dir_name, '*.wmf'))
+
+def parseDoDoc_XML(xml_content, file_mappings):
+    p = MappingParser(file_mappings)
+    xml.sax.parseString(xml_content, p)
+    return expandImages_in_tables(p.tree)
+    return p.tree
+
+class ThreadingHTTPServer(ThreadingMixIn, BaseHTTPServer.HTTPServer):
+    """HTTP server, that uses multiple processes to handle requests"""
+
+def _bare_address_string(self):
+    host, port = self.client_address[:2]
+    return str(host)
+
+BaseHTTPServer.BaseHTTPRequestHandler.address_string = _bare_address_string
 
 def main():
     try:
-        server = HTTPServer(('', 80), MyHandler)
+        #server = HTTPServer(('', 80), MyHandler)
+        server = ThreadingHTTPServer(('', 80), MyHandler)
         print 'started httpserver...'
         server.serve_forever()
     except KeyboardInterrupt:
